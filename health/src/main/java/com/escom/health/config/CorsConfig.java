86d970baaa1fd8a766175acdd8f9010fc9c191a8
Jonathan Luciano <129464379+JonathanLuher@users.noreply.github.com>
2025-03-23 15:57:15 -0600

    Add CorsConfig.java
    On this commit I added a file to conect the API in springboot to frontend in React

---
+++ health/src/main/java/com/escom/health/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.escom.health.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**") // Permitir todas las rutas
+                        .allowedOrigins("http://localhost:5173") // Permitir el frontend de Vite
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
